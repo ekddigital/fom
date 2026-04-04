fc4f6bcc42dbc9924da6084fc252f4e1e440c92c
EKD Digital <ekddigital@ekddigital.com>
2026-04-04 21:38:24 +0800

    feat: add JICF Easter Winner Certificate 2026 with pastor signature

--- lib/utils/certificates/jicf/index.ts
+++ lib/utils/certificates/jicf/index.ts
@@ -4,8 +4,10 @@
 
 export * from "./constants";
 export * from "./sample";
+export * from "./easter-winner";
 
 // Re-export all templates as an array for easy access
 import { jicfSampleCert } from "./sample";
+import { jicfEasterWinnerCert } from "./easter-winner";
 
-export const JICF_TEMPLATES = [jicfSampleCert];
+export const JICF_TEMPLATES = [jicfSampleCert, jicfEasterWinnerCert];
